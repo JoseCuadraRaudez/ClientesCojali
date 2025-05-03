61c82532784db16c26cca92cad4c8eb9481e0f8a
JoseCuadra <josetcuadra@gmail.com>
2025-05-02 21:54:23 -0600

    Resolucion al Ejercicio 2

---
+++ src/Infrastructure/Repositories/MySqlUsuarioRepository.cs
@@ -0,0 +1,43 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+using Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories
+{
+    public class MySqlUsuarioRepository : IUsuarioRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MySqlUsuarioRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Usuario>> GetAllAsync() => await _context.Usuarios.ToListAsync();
+
+        public async Task<Usuario> GetByIdAsync(Guid id) => await _context.Usuarios.FindAsync(id);
+
+        public async Task AddAsync(Usuario usuario)
+        {
+            _context.Usuarios.Add(usuario);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Usuario usuario)
+        {
+            _context.Usuarios.Update(usuario);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(Guid id)
+        {
+            var usuario = await _context.Usuarios.FindAsync(id);
+            if (usuario != null)
+            {
+                _context.Usuarios.Remove(usuario);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
